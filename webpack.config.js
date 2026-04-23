60cd58c6437031896b5fa07ad8bc8c620ac67cad
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-04-22 22:50:37 -0500

    add webpack stuff I don't understand

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 import path from "node:path";
+import { fileURLToPath } from 'node:url';
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 export default {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(import.meta.dirname,"dist"),
-        clean: true,
+    mode: "production",
+    entry: {
+        app: "./src/index.js",
     },
     devtool: "eval-source-map",
     devServer: {
@@ -16,13 +20,18 @@ export default {
     plugins: [
     new HtmlWebpackPlugin({
         template: "./src/template.html",
+        title: "Production",
+        }),
+    new MiniCssExtractPlugin({
+          filename: "[name].css",
+          chunkFilename: "[id].css",
         }),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader","css-loader"],
+                use: ["style-loader",MiniCssExtractPlugin.loader,"css-loader"],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -34,5 +43,28 @@ export default {
             },
 
         ],
+    },
+    optimization: {
+        minimizer: [
+          // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`).
+          // Uncomment the next line to keep JS minimizers and add CSS minimizer:
+          `...`,
+          new CssMinimizerPlugin(),
+        ],
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: "styles",
+                    type: "css/mini-extract",
+                    chunks: "all",
+                    enforce: true,
+                },
+            },
+        },
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname,"dist"),
+        clean: true,
     },
 };
